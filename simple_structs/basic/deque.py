f514846e3a3af2a941d3b8ab268c12a37177d424
k1t1k <nikitabirukov@gmail.com>
2019-12-12 23:46:08 +0300

    change deque

--- simple_structs/basic/deque.py
+++ simple_structs/basic/deque.py
@@ -19,3 +19,12 @@ class Deque():
 
     def removeRear(self):
         return self.items.pop(0)
+
+
+def listsum(numList):
+   if len(numList) == 1:
+        return numList[0]
+   else:
+        return numList[0] + listsum(numList[1:])
+
+print(listsum([1,3,5,7,9]))
